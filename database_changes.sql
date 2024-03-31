df56b8b83f85c158e6012e60ecd3a1a7b2716a8c
jLevar <joshlevar@gmail.com>
2024-03-30 18:14:55 -0600

    Version 1.1 (+SQL Changes (v2))

--- database_changes.sql
+++ database_changes.sql
@@ -15,3 +15,10 @@ LEFT JOIN assets AS b ON a.user_id = b.user_id;
 
 
 ALTER TABLE account DROP COLUMN balance;
+
+INSERT INTO assets (entity_id, cash, gold, silver)
+VALUES
+    ("BK", 1000000000, 0, 0),
+    ("GOV", 1000000000000, 0, 0),
+    ("CASINO", 1000000000, 0, 0),
+    ("TERRA", 0, 1000000, 10000000);
